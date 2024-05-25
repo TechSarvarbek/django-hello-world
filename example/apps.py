71ab7e88f7f5a8aa708f99cb813e0a5919b61d26
techSarvarbek <techSarvarbek@gmail.com>
2024-05-25 23:37:07 +0500

    c

--- example/apps.py
+++ example/apps.py
@@ -1,10 +1,6 @@
 from django.apps import AppConfig
-import os
 
-class YourAppConfig(AppConfig):
-    name = 'example'  # Replace with your app name
 
-    def ready(self):
-        from django.core.management import call_command
-        if os.environ.get('RUN_MAIN', None) != 'true':  # Prevent from running twice with autoreload
-            call_command('runserver_bot')
+class ExampleConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'example'
